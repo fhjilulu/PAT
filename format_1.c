615f4adbace108a2a49a8309f962a1c7a033a921
jilulu <fhjilulu@163.com>
2020-09-22 19:28:43 +0800

    master

---
+++ format_1.c
@@ -0,0 +1,13 @@
+//Calculate a+b and output the sum in standard format -- that is, 
+//the digits must be separated into groups of three by commas (unless there are less than four digits).
+
+//计算a+b并用标准格式输出和，数字每三个用逗号隔开（除非数字小于四位）
+
+
+#include<stdio.h>
+int main()
+{
+    
+}
+
+
